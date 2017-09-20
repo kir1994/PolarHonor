984755c6f7589677abd91493ad24467ee2a36c68
kir1994 <antimozgpunk@gmail.com>
2017-09-21 01:58:27 +0200

    ~ refactoring + more advanced tester ~ best constellation in result

---
+++ HonorCup/Config.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <vector>
+#include <complex>
+#include <random>
+
+using namespace std;
+
+const double TARGET_CAPACITY = 3.6;
+const unsigned NUM_OF_ITERATIONS = 10000;
+const double EPS = 1e-3;
+const unsigned CONSTELLATION_SIZE = 16;
+const double SMALL_MODULE = 0.1;
+
+const double TARGET_SNR = 12;
+const double TARGET_SIGMA = sqrt(1. / (2 * pow(10., TARGET_SNR / 10)));
+
+const double SCALING_FACTOR = 3;
+const double CAPACITY_SEED = 123456487;
+
+
+extern vector<complex<double> > circle16;
+extern vector<complex<double> > hex16;
+extern vector<complex<double> > opt16;
+extern vector<complex<double> > apsk12_4;
+extern vector<complex<double> > qam16;
+
+extern mt19937_64 gen;
+extern vector<double> noiseTable;
+extern vector<unsigned> uniformTable;
+
+enum handle_bad_points { DO_NOTHING, MOVE_POINT, MOVE_CONSTELLATION };
+void Normalize(vector<complex<double> >& Constellation, handle_bad_points mode = DO_NOTHING, double eps = 0.01);
+
+double avgPower(const vector<complex<double> >& Constellation);
+double CapacityApprox(const vector<complex<double> >& Constellation, double Sigma, unsigned NumOfIterations);
+double FindSNR(const vector<complex<double> >& Constellation);
+
+vector<complex<double> > DEConstellationSearch(
+	unsigned ConstellationSize, bool UseBest,
+	unsigned PopulationSize, double CrossoverProbability, double AmplificationFactor,
+	unsigned NumOfGenerations);
+
+void PrintConstellation(const vector<complex<double> >& Constellation);
+
+void handler(int sig);
--- Tester/Source.cpp
+++ Tester/Source.cpp
@@ -1,94 +1,49 @@
 #include <fstream>
 #include <iostream>
 #include <complex>
-#include <vector>
-#include <random>
 #include <ctime>
 #include <algorithm>
 
-using namespace std;
+#include "../HonorCup/Config.h"
 
-uniform_real_distribution<double> distr;
-auto gen = mt19937_64(time(nullptr));
+normal_distribution<double> noiseD(0., TARGET_SIGMA);
+uniform_int_distribution<int> distr(0., CONSTELLATION_SIZE - 1);
 
-const double TARGET_CAPACITY = 3.6;
-const unsigned NUM_OF_ITERATIONS = 100000;
-const double EPS = 1e-4;
-const unsigned CONSTELLATION_SIZE = 16;
-
-double avgPower(const vector<complex<double> >& Constellation)
+void PrintStat(const vector < complex < double > >& Constellation)
 {
-	double res = 0;
-
-	for (auto elem : Constellation)
-		res += pow(abs(elem), 2) / Constellation.size();
-
-	return res;
+	cout << "Power: ";
+	cerr << avgPower(Constellation) << "\t";
+	cout << endl;
+	double m = HUGE_VAL;
+	for (auto&& elem : Constellation)
+		m = min(m, abs(elem));
+	cout << "Min abs: ";
+	cerr << m << "\t";
+	cout << endl << "Capacity at 12dB: ";
+	cerr << CapacityApprox(Constellation, TARGET_SIGMA, NUM_OF_ITERATIONS) << "\t";
+	cout << endl << "SNR for 3.6 bits/symbol: ";
+	cerr << FindSNR(Constellation);
 }
-
-
-
-double CapacityApprox(const vector<complex<double> >& Constellation, double Sigma, unsigned NumOfIterations)
-{
-	uniform_int_distribution<int> distr(0., Constellation.size() - 1);
-	normal_distribution<double> noiseD(0., Sigma);
-	vector<double> probs(Constellation.size());
-
-	double globalSum = 0;
-
-	for (unsigned i = 0; i < NumOfIterations; ++i)
-	{
-		int elem = distr(gen);
-		complex<double> noise = { noiseD(gen),  noiseD(gen) };
-		complex<double> transmitted = Constellation[elem] + noise;
-
-
-		double sum = 0;
-
-		for (unsigned j = 0; j < Constellation.size(); ++j)
-		{
-			probs[j] = exp(-(pow(abs(transmitted - Constellation[j]), 2) - pow(abs(noise), 2)) / (2 * Sigma * Sigma));
-			sum += probs[j];
-		}
-		for (unsigned j = 0; j < Constellation.size(); ++j)
-			probs[j] /= sum;
-
-		double mutualInf = 0.;
-
-		for (unsigned j = 0; j < Constellation.size(); ++j)
-			if (probs[j] != 0)
-				mutualInf += probs[j] * log2(probs[j]);
-		globalSum += mutualInf;
-	}
-
-	return globalSum / NumOfIterations + log2(Constellation.size());
-}
-
-double FindSNR(const vector<complex<double> >& Constellation)
-{
-	double leftSNR = 10.;
-	double rightSNR = 25;
-
-	while (fabs(rightSNR - leftSNR) > EPS)
-	{
-		double SNR = (rightSNR + leftSNR) / 2;
-		double Sigma = sqrt(1. / (2 * pow(10., SNR / 10)));
-		double C = CapacityApprox(Constellation, Sigma, NUM_OF_ITERATIONS);
-
-		if (C - TARGET_CAPACITY > EPS)
-			rightSNR = SNR;
-		else if (TARGET_CAPACITY - C > EPS)
-			leftSNR = SNR;
-		else
-			return SNR;
-	}
-	return leftSNR;
-}
-
 
 void main(int argc, char ** argv)
 {
 	ifstream ifs(argv[1]);
+	double eps = 0.01;
+	string mode = "";
+	if (argc > 2)
+	{
+		eps = stod(argv[2]);
+		if (argc > 3)
+			mode = argv[3];
+	}
+	gen.seed(CAPACITY_SEED);
+	for (unsigned i = 0; i < 2 * NUM_OF_ITERATIONS; ++i) {
+		noiseTable[i] = noiseD(gen);
+	}
+	for (unsigned i = 0; i < NUM_OF_ITERATIONS; ++i) {
+		uniformTable[i] = distr(gen);
+	}
+
 
 	if (!ifs)
 		cout << "Cannot open file\n";
@@ -97,11 +52,22 @@ void main(int argc, char ** argv)
 		vector < complex < double > > Constellation(CONSTELLATION_SIZE);
 		for (auto&& elem : Constellation)
 			ifs >> elem._Val[0] >> elem._Val[1];
-		cout << "Power: " << avgPower(Constellation) << endl;
-		double m = 100.;
-		for (auto&& elem : Constellation)
-			m = min(m, abs(elem));
-		cout << "Min abs: " << m << endl;
-		cout << FindSNR(Constellation);
+		cout << "Original constellation:\n";
+		PrintStat(Constellation);
+
+		cout << endl << "\nMove point with eps " << eps << ": \n";
+		auto tmpConst = Constellation;
+		Normalize(tmpConst, MOVE_POINT, eps);
+		PrintStat(tmpConst);
+
+		if (mode == "--dumppoint")
+			PrintConstellation(tmpConst);
+
+		cout << endl << "\nMove constellation with eps " << eps << ": \n";
+		tmpConst = Constellation;
+		Normalize(tmpConst, MOVE_CONSTELLATION, eps);
+		PrintStat(tmpConst);
+		if (mode == "--dumpconst")
+			PrintConstellation(tmpConst);
 	}
 }
